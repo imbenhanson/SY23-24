3812a71ee974f31373c4209c8a392a5f4b3c1612
benhanson <bphan178@learner.dvusd.org>
2024-03-21 13:39:22 -0700

    nolan push!!
    yes!!

--- nolan burnell cain/nolan burnell cain/Form1.vb
+++ nolan burnell cain/nolan burnell cain/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Dim f2 As New Form2
+        f2.ShowDialog Then
+            Label2.Text = f2.amount.value
+        Me.Show()
+    End Sub
 End Class
