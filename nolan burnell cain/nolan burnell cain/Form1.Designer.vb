7633081e9291946e1dc95e86524b451574c518fb
Nolan Cain <nbcai604@learner.dvusd.org>
2024-03-21 14:01:31 -0700

    aaa

--- nolan burnell cain/nolan burnell cain/Form1.Designer.vb
+++ nolan burnell cain/nolan burnell cain/Form1.Designer.vb
@@ -23,28 +23,53 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.amount = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(443, 289)
+        Me.Button1.Location = New System.Drawing.Point(197, 148)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(94, 29)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'amount
+        '
+        Me.amount.AutoSize = True
+        Me.amount.Location = New System.Drawing.Point(195, 329)
+        Me.amount.Name = "amount"
+        Me.amount.Size = New System.Drawing.Size(53, 20)
+        Me.amount.TabIndex = 1
+        Me.amount.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(374, 205)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1115, 615)
+        Me.ClientSize = New System.Drawing.Size(1133, 621)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.amount)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents amount As Label
+    Friend WithEvents Label2 As Label
 End Class
